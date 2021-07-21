7d82b3a159912ab15da79fda932c9bf02c93b01e
gyan <gyan9427@gmail.com>
2021-07-21 23:23:45 +0530

    created initial components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {Link,Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <Link to='/login'>Login</Link>
+      
     </div>
   );
 }
